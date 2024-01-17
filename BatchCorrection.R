8dfc7ede4d4a9f0315c185ed101611110fa5dcf0
CGnTLab <105309607+CGnTLab@users.noreply.github.com>
2024-01-17 11:09:42 +0530

    Update BatchCorrection.R

--- BatchCorrection.R
+++ BatchCorrection.R
@@ -1,7 +1,10 @@
 ########################
 ### Batch Correction ###
 ########################
-
+## Batch correction in RNA-Seq data involves mitigating technical variations or batch effects
+## introduced during sample processing. Methods like ComBat or surrogate variable analysis are 
+## employed to adjust gene expression values, ensuring more accurate comparisons and reliable 
+## downstream analyses by minimizing batch-related variability.
 library(sva)
 
 df<-read.csv("input.csv")
